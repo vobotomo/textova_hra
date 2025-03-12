5f4b2b4694f98ea81d922d4ce258cb7832bd637d
vobotomo <vobornyt@gmail.com>
2025-03-12 10:52:30 +0100

    Trida Konzole : metody inicializace, provedPrikaz

--- src/Konzole.java
+++ src/Konzole.java
@@ -1,2 +1,42 @@
+import command.*;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Konzole {
+
+    private Scanner sc = new Scanner(System.in);
+    private boolean exit = false;
+    private HashMap<String, Command> prikazy;
+
+
+    public void inicializace() {
+        prikazy = new HashMap<>();
+        prikazy.put("exit", new Exit());
+        prikazy.put("mluv", new Mluv());
+        prikazy.put("napoveda", new Napoveda());
+        prikazy.put("pomoc", new Pomoc());
+        prikazy.put("pouzit", new Pouzit());
+        prikazy.put("prozkoumej", new Prozkoumej());
+        prikazy.put("vyber", new Vyber());
+        prikazy.put("vzit", new Vzit());
+        prikazy.put("zahod", new Zahod());
+        //prikazy.put("jdiDo", new JdiDo());
+    }
+
+    public void provedPrikaz() {
+        System.out.println(">");
+        String prikaz = sc.next();
+        if (prikazy.containsKey(prikaz)) {
+            System.out.println(prikazy.get(prikaz).execute());
+            exit = prikazy.get(prikaz).exit();
+        }else{
+            System.out.println("Neplatny prikaz!");
+        }
+    }
+
+
+
+
+
 }
